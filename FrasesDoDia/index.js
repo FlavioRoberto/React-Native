d69a69f7237a29a10a7fbd9f601bb93465cf4f8c
Flávio Roberto Teixeira <flaviolive3@hotmail.com>
2018-06-28 14:08:55 -0300

    Finalização app frase do dia

--- FrasesDoDia/index.js
+++ FrasesDoDia/index.js
@@ -5,7 +5,22 @@ import {
   Image,
   TouchableOpacity,
   AppRegistry,
+  Alert
 } from 'react-native';
+
+const frasesDoDia = [
+    'A motivação não é a fórmula mágica para atingir o sucesso, mas certamente garante o caminho mais rápido para o alcançar.',
+    'Você não só tem o direito de ser feliz como também tem a obrigação de lutar para alcançar a felicidade.',
+    'Não perca a motivação só porque as coisas não estão correndo como o previsto. Adversidade gera sabedoria e é isso que levará você ao sucesso.',
+    'Encare o que fez de errado com motivação, pois é isso que o ajudará a fazer certo da próxima vez.',
+    'Toda ação humana, quer se torne positiva ou negativa, precisa depender de motivação.'
+];
+
+const gerarNovaFrase = ()=>{
+    var numeroAleatorio = Math.random();
+    numeroAleatorio = Math.floor(numeroAleatorio * 5);
+    return Alert.alert("Frase do dia",frasesDoDia[numeroAleatorio]);
+};
 
 const estilos = {
   principal: {
@@ -34,7 +49,7 @@ const App = () => {
   return (
     <View style={principal}>
       <Image source={require('./imgs/logo.png')} />
-      <TouchableOpacity style={botao}>
+      <TouchableOpacity style={botao} onPress={gerarNovaFrase}>
         <Text style={textoBotao}>Nova Frase</Text>
       </TouchableOpacity>
     </View>
